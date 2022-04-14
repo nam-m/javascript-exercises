b37fc86758f3ee5599a9e453265c5c5296c71615
Nam Anh Mai <maithenamanh@gmail.com>
2022-04-14 01:08:09 -0700

    finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0)
+        return 'ERROR';
+    else if (num == 0)
+        return '';
+    else {
+        let newString = '';
+        for (let i = 0; i < num; i++)
+            newString += string;
+        return newString;
+    }
 };
 
 // Do not edit below this line
