face6620fc93a974ece93937ccb907d4ac4077e9
Nam Anh Mai <maithenamanh@gmail.com>
2022-04-15 01:10:59 -0700

    finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    outer: if (typeof(start) == 'number' && typeof(end) == 'number') {
+        if (start < 0 || end < 0)
+            break outer;
+        if (start > end) {
+            let temp = start;
+            start = end;
+            end = temp;
+        }
 
+        /* Normal way
+           -------------------------------------------*/
+        // let sum = 0;
+        // for (let i = start; i <= end; i++)
+        //     sum += i;
+        // return sum;
+
+        /* Smart way
+           -------------------------------------------*/
+        return (end - start + 1) * (start + end) / 2;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
