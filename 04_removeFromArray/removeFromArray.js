054badfa542473c17ff49b284621e3956233a549
Nam Anh Mai <maithenamanh@gmail.com>
2022-04-15 00:12:48 -0700

    finished 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args)
+        for (const item of arr)
+            if (item === arg)
+                arr.splice(arr.indexOf(item), 1);
+    return arr;
 };
 
 // Do not edit below this line
