e0627e35221485553093b948c7ffe9b95a5224d9
Nam Anh Mai <maithenamanh@gmail.com>
2022-05-26 22:22:15 -0700

    finished 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, item) => product * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	return Array(num).fill(null)
+                   .map((current, index) => index + 1)
+                   .reduce((product, item) => product * item, 1);
 };
 
 // Do not edit below this line
