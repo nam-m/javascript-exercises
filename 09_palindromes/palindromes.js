0f4ea507b632601b7af3e3fecb1b06ed10443be8
Nam Anh Mai <maithenamanh@gmail.com>
2022-05-27 00:59:56 -0700

    finished 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = str.toLowerCase().replace(/\W/g, '');
+    // console.log(newStr);
+    return (
+        newStr.split('')
+              .reverse()
+              .join('') == newStr
+    );
 };
 
 // Do not edit below this line
